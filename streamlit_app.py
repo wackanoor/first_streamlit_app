d719215964181f1c58d0746178906d800c244f73
wackanoor <125065693+wackanoor@users.noreply.github.com>
2023-05-03 19:49:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,16 @@
 import streamlit
 
+
 streamlit.title(' wohooo !!! My first Streamlit Program')
 
 streamlit.header('Menu')
 streamlit.text('🐔 Chicken curry')
 streamlit.text('🥣 Paneer curry')
 streamlit.text('🍞 Chapati')
+
+streamlit.title('Build your own smoothie')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
+
