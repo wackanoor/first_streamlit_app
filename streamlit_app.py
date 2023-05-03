fa7991e6f3f593767a7df9fd7422adaba38bc81a
wackanoor <125065693+wackanoor@users.noreply.github.com>
2023-05-03 19:32:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title(' wohooo !!! My first Streamlit Program')
+
+streamlit.header('Menu')
+streamlit.text('Chicken curry')
+streamlit.text('Paneer curry')
+streamlit.text('Chapati')
