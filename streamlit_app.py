854901c7a9b0ccc743de15ff540113a81195615e
wackanoor <125065693+wackanoor@users.noreply.github.com>
2023-05-05 16:26:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,19 @@ import requests
 streamlit.header('Fruityvice fruit advice!!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+#normalize json data
+fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
+
+
+
+
+
+
+
+
+
+
+
+
